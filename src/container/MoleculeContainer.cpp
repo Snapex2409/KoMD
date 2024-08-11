6b96095f851c754bf67471ea9ef1fae8a3d1de73
Alex Hocks <alex.hocks@tum.de>
2024-08-11 20:05:33 +0200

    add iterators

--- src/container/MoleculeContainer.cpp
+++ src/container/MoleculeContainer.cpp
@@ -92,6 +92,22 @@ void MoleculeContainer::updateContainer() {
     clearForces();
 }
 
+MoleculeContainer::Iterator MoleculeContainer::iterator(const IteratorType type, const IteratorRegion region) {
+    math::ul3 low, high;
+    if (region == DOMAIN) {
+        low = {1, 1, 1};
+        high = m_data.dims() - 2;
+    }
+    if (region == DOMAIN_HALO) {
+        low = {0, 0, 0};
+        high = m_data.dims() - 1;
+    }
+
+    if (type == MOLECULE) writeSOA2AOS();
+
+    return {low, high, type == MOLECULE, m_data};
+}
+
 void MoleculeContainer::constructSOAs() {
     if (!Registry::instance->configuration()->enableSOA) return;
 
@@ -157,4 +173,133 @@ void MoleculeContainer::clearForces() {
             }
         }
     }
+}
+
+MoleculeContainer::Iterator::Iterator(const math::ul3 &min, const math::ul3 &max, bool only_mol, Vec3D<Cell> &cells) :
+m_cell_coord(min), m_cell_min(min), m_cell_max(max), m_only_molecule(only_mol),
+m_site_idx(0), m_molecule_idx(0), m_visited_sites_cell(0), m_cells(cells), m_cell_has_soa(cells[min].validSOA()) {
+    if (!cells[min].molecules().empty()) return;
+
+    // cell is empty, find the first cell in iteration direction that is not empty
+    findNextCell();
+    m_cell_has_soa = cells[m_cell_coord].validSOA();
+}
+
+void MoleculeContainer::Iterator::operator++() {
+    Cell& cell = m_cells[m_cell_coord];
+    auto& cell_molecules = cell.molecules();
+
+    // move to next site
+    m_visited_sites_cell++;
+    if (!m_only_molecule) {
+        if (m_site_idx < cell_molecules[m_molecule_idx].getSites().size() - 1) {
+            m_site_idx++;
+            return;
+        }
+    }
+
+    // move to next molecule
+    if (m_molecule_idx < cell_molecules.size() - 1) {
+        m_molecule_idx++;
+        m_site_idx = 0;
+        return;
+    }
+
+    // move to next cell
+    findNextCell();
+    m_cell_has_soa = m_cells[m_cell_coord].validSOA();
+}
+
+bool MoleculeContainer::Iterator::isValid() {
+    // check if cell is invalid
+    if (m_cell_coord.x() == m_cell_max.x() + 1 &&
+        m_cell_coord.y() == m_cell_max.y() + 1 &&
+        m_cell_coord.z() == m_cell_max.z() + 1) return false;
+
+    Cell& cell = m_cells[m_cell_coord];
+    auto& cell_molecules = cell.molecules();
+    // cell is valid
+    // check if current molecule has more sites
+    if (!m_only_molecule) // only do this if we iterate over sites
+        if (m_site_idx < cell_molecules[m_molecule_idx].getSites().size() - 1) return true;
+
+    // no more sites
+    // check if there is another molecule in this cell
+    if (m_molecule_idx < cell_molecules.size() - 1) return true;
+
+    // no more molecules in this cell
+    // check if there is another cell with molecules
+    bool init = false;
+    for (uint64_t z = m_cell_coord.z(); z <= m_cell_max.z(); z++) {
+        for (uint64_t y = init ? m_cell_min.y() : m_cell_coord.y(); y <= m_cell_max.y(); y++) {
+            for (uint64_t x = init ? m_cell_min.x() : m_cell_coord.x(); x <= m_cell_max.x(); x++) {
+                if (m_cells[x, y, z].molecules().empty()) continue;
+                // found non empty cell
+                return true;
+            }
+            init = true;
+        }
+    }
+
+    // all checks failed
+    return false;
+}
+
+math::d3 & MoleculeContainer::Iterator::f() {
+    if (m_cell_has_soa) return m_cells[m_cell_coord].soa().f()[m_visited_sites_cell];
+    return m_cells[m_cell_coord].molecules()[m_molecule_idx].getSites()[m_site_idx].f_arr();
+}
+
+math::d3 & MoleculeContainer::Iterator::r() {
+    if (m_cell_has_soa) return m_cells[m_cell_coord].soa().r()[m_visited_sites_cell];
+    return m_cells[m_cell_coord].molecules()[m_molecule_idx].getSites()[m_site_idx].r_arr();
+}
+
+math::d3 & MoleculeContainer::Iterator::v() {
+    if (m_cell_has_soa) return m_cells[m_cell_coord].soa().v()[m_visited_sites_cell];
+    return m_cells[m_cell_coord].molecules()[m_molecule_idx].getSites()[m_site_idx].v_arr();
+}
+
+double MoleculeContainer::Iterator::epsilon() {
+    if (m_cell_has_soa) return m_cells[m_cell_coord].soa().epsilon()[m_visited_sites_cell];
+    return m_cells[m_cell_coord].molecules()[m_molecule_idx].getSites()[m_site_idx].getEpsilon();
+}
+
+double MoleculeContainer::Iterator::sigma() {
+    if (m_cell_has_soa) return m_cells[m_cell_coord].soa().sigma()[m_visited_sites_cell];
+    return m_cells[m_cell_coord].molecules()[m_molecule_idx].getSites()[m_site_idx].getSigma();
+}
+
+double MoleculeContainer::Iterator::mass() {
+    if (m_cell_has_soa) return m_cells[m_cell_coord].soa().mass()[m_visited_sites_cell];
+    return m_cells[m_cell_coord].molecules()[m_molecule_idx].getSites()[m_site_idx].getMass();
+}
+
+uint64_t MoleculeContainer::Iterator::ID() {
+    if (m_cell_has_soa) return m_cells[m_cell_coord].soa().id()[m_visited_sites_cell];
+    return m_cells[m_cell_coord].molecules()[m_molecule_idx].ID();
+}
+
+Molecule & MoleculeContainer::Iterator::molecule() {
+    return m_cells[m_cell_coord].molecules()[m_molecule_idx];
+}
+
+void MoleculeContainer::Iterator::findNextCell() {
+    bool init = false;
+    for (uint64_t z = m_cell_coord.z(); z <= m_cell_max.z(); z++) {
+        for (uint64_t y = init ? m_cell_min.y() : m_cell_coord.y(); y <= m_cell_max.y(); y++) {
+            for (uint64_t x = init ? m_cell_min.x() : (m_cell_coord.x()+1); x <= m_cell_max.x(); x++) {
+                if (m_cells[x, y, z].molecules().empty()) continue;
+                // found non empty cell
+                m_cell_coord = {x, y, z};
+                m_molecule_idx = 0;
+                m_site_idx = 0;
+                m_visited_sites_cell = 0;
+                return;
+            }
+            init = true;
+        }
+    }
+    // if loop is not successful cell_coord = max+1 => isValid becomes false
+    m_cell_coord = m_cell_max + 1;
 }
--- src/container/MoleculeContainer.h
+++ src/container/MoleculeContainer.h
@@ -23,6 +23,47 @@ public:
      * */
     math::ul3 findCell(const math::d3& pos, bool& valid);
     void writeSOA2AOS();
+
+    struct Iterator {
+    public:
+        Iterator(const math::ul3& min, const math::ul3& max, bool only_mol, Vec3D<Cell>& cells);
+        void operator++();
+        bool isValid();
+
+        math::d3& f();
+        math::d3& r();
+        math::d3& v();
+        double epsilon();
+        double sigma();
+        double mass();
+        uint64_t ID();
+
+        Molecule& molecule();
+    private:
+        void findNextCell();
+
+        math::ul3 m_cell_coord;
+        const math::ul3 m_cell_min;
+        const math::ul3 m_cell_max;
+        const bool m_only_molecule;
+        uint64_t m_site_idx;
+        uint64_t m_molecule_idx;
+        uint64_t m_visited_sites_cell;
+        Vec3D<Cell>& m_cells;
+        bool m_cell_has_soa;
+    };
+
+    enum IteratorType {
+        MOLECULE,
+        SITE
+    };
+
+    enum IteratorRegion {
+        DOMAIN,
+        DOMAIN_HALO
+    };
+
+    Iterator iterator(const IteratorType type, const IteratorRegion region);
 private:
     Vec3D<Cell> m_data;
     void constructSOAs();
